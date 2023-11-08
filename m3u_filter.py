7d26f5b67e856c89f01c27e34c69c11426003451
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2023-11-08 22:23:41 +0100

    Update m3u_filter.py

--- m3u_filter.py
+++ m3u_filter.py
@@ -2,10 +2,11 @@ from datetime import date
 import re
 
 input_file = "input.m3u"
-include_adult = False
+include_adult = True
 
 patterns = [",AT |", ",DE |"]
 adult_pattern = ",XXX |"
+
 
 def process_pattern_name(pattern_name):
     return re.sub(r"[^a-zA-Z]", "", pattern_name)
@@ -19,16 +20,15 @@ def get_pattern_names(patterns):
 
 
 def create_file_name(patterns):
-    return date.today().strftime("%Y-%m-%d") + "_" + get_pattern_names(patterns) + ".m3u"
+    return (
+        date.today().strftime("%Y-%m-%d") + "_" + get_pattern_names(patterns) + ".m3u"
+    )
 
 
 def filter_m3u(input_file, output_file, patterns: list):
-    if include_adult:
-        patterns.append(adult_pattern)
     with open(input_file, "r") as infile, open(output_file, "w") as outfile:
         lines = infile.readlines()
         keep_next_line = False
-
         for line in lines:
             if line.startswith("#EXTM3U"):
                 outfile.write(line)
@@ -43,4 +43,6 @@ def filter_m3u(input_file, output_file, patterns: list):
 
 filter_m3u("./" + input_file, create_file_name(patterns), patterns)
 
-# edit the script so it prepares two output lists, one with XXX and one without
+if include_adult:
+    patterns.append(adult_pattern)
+    filter_m3u("./" + input_file, create_file_name(patterns), patterns)
