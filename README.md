95d8856d7c7c2f27cd996d0e92f019f5d5e9ac63
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2024-03-20 23:42:52 +0100

    added sorting function; remove pattern names from channel names

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # m3u_filter
 
-This python script takes an .m3u file as input. The file is filtered by the specified patterns (filters). Afterwards the result will be written into a new .m3u file (output).
-
-The output file is renamed to the current date and the filter-patterns that were used during processing.
+This simple python script takes an .m3u file as input. The file is filtered by the specified patterns (include and exclude pattern). Afterwards the pattern names will be removed from the channel names. Finally the result will be sorted and written into a new .m3u file (output).
 
 Main purpose of the script is to filter .m3u lists by location reference.
--- m3u_filter.py
+++ m3u_filter.py
@@ -1,48 +1,53 @@
-from datetime import date
-import re
-
 input_file = "input.m3u"
-include_adult = True
+output_file = "output.m3u"
 
-patterns = [",AT |", ",DE |"]
-adult_pattern = ",XXX |"
-
-
-def process_pattern_name(pattern_name):
-    return re.sub(r"[^a-zA-Z]", "", pattern_name)
+include_patterns = ["DE: "]
+exclude_patterns = ["OSOKOYO", "VIP-DE", "XXX"]
 
 
-def get_pattern_names(patterns):
-    pattern_names = []
-    for pattern_name in patterns:
-        pattern_names.append(process_pattern_name(pattern_name))
-    return "_".join(pattern_names)
+# maybe need to edit the get_tvg_name function to match your m3u file;
+# the tvg-name attribute is not always included in the m3u file
+def get_tvg_name(line: str):
+    return line.split('tvg-name="')[1].split('"')[0]
 
 
-def create_file_name(patterns):
-    return (
-        date.today().strftime("%Y-%m-%d") + "_" + get_pattern_names(patterns) + ".m3u"
-    )
+def rename_channel(line: str, patterns: list):
+    for pattern in patterns:
+        line = line.replace(pattern, "")
+    return line
 
 
-def filter_m3u(input_file, output_file, patterns: list):
+def filter_m3u(
+    input_file: str, output_file: str, inc_patterns: list, exc_patterns: list
+):
     with open(input_file, "r") as infile, open(output_file, "w") as outfile:
         lines = infile.readlines()
-        keep_next_line = False
-        for line in lines:
-            if line.startswith("#EXTM3U"):
-                outfile.write(line)
+
+        outfile.write("#EXTM3U\n")
+        filtered_pairs = []
+        line_pairs = [(lines[i], lines[i + 1]) for i in range(1, len(lines), 2)]
+
+        for pair in line_pairs:
+            if not (any(pattern in pair[0] for pattern in inc_patterns)):
                 continue
-            if any(pattern in line for pattern in patterns):
+            if any(pattern in pair[0] for pattern in exc_patterns):
+                continue
+            filtered_pairs.append(pair)
+
+        renamed_pairs = []
+
+        for pair in filtered_pairs:
+            renamed_pairs.append((rename_channel(pair[0], inc_patterns), pair[1]))
+
+        sorted_pairs = sorted(renamed_pairs, key=lambda pair: get_tvg_name(pair[0]))
+        for line_pair in sorted_pairs:
+            for line in line_pair:
                 outfile.write(line)
-                keep_next_line = True
-            elif keep_next_line:
-                outfile.write(line)
-                keep_next_line = False
 
 
-filter_m3u("./" + input_file, create_file_name(patterns), patterns)
-
-if include_adult:
-    patterns.append(adult_pattern)
-    filter_m3u("./" + input_file, create_file_name(patterns), patterns)
+filter_m3u(
+    "./" + input_file,
+    output_file,
+    include_patterns,
+    exclude_patterns,
+)
